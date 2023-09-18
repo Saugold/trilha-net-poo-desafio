730252b41864e914f43a1a3ddf1878e14a48021a
Douglas Alexandrino <douglx9898@gmail.com>
2023-09-18 19:58:05 -0300

    Resolução

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,15 +2,29 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get; set; }
+        private string Modelo {get;set;}
+        private string IMEI{get; set;}
+        private int Memoria{get;set;}
+        
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            
         }
-
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Numero:{Numero}");
+            Console.WriteLine($"Modelo:{Modelo}");
+            Console.WriteLine($"IMEI:{IMEI}");
+            Console.WriteLine($"Memoria:{Memoria}");
+        }
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("132","Nokia","1345ab21",8);
+Iphone iphone = new Iphone("456","Iphone","34560lkd3k",4);
+Console.WriteLine("Nokia---------------------------------------");
+nokia.MostrarInfo();
+nokia.InstalarAplicativo("Instagram");
+Console.WriteLine("Iphone---------------------------------------");
+iphone.MostrarInfo();
+iphone.InstalarAplicativo("WhatsApp");
